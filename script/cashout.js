705e21cb8fa56d9f4be6b932e1d42e6b1274bac2
MD-Abdullah <abdullah0153757@gmail.com>
2025-04-27 03:36:18 +0600

    cashout complete

---
+++ script/cashout.js
@@ -0,0 +1,24 @@
+document.getElementById('cashout-btn').addEventListener('click', function(event){
+    event.preventDefault();
+    const accountNumber = document.getElementById('account-number').value;
+
+    const amount = getInputValueByID('cashout-amount');
+    const pinNumber = getInputValueByID('cashout-pin');
+    const mainBalance = getInnerTextByID('main-balance');
+    
+
+if(accountNumber.length === 11){
+    if(pinNumber === 1234){
+        const sum = mainBalance - amount;
+        getInnerTextByIDandValue('main-balance', sum)
+
+    }
+    else{
+        alert('Pin not valid')
+    }
+
+}
+else{
+    alert('Account number not valid')
+}
+})
